8cd3ef78605e10868ee3d843b1a838564dec6b37
amritbharadwaj-dev <theunkhownman999@gmail.com>
2026-09-13 15:05:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const booksData = [
         title: { en: 'Accountancy Part I (Financial Accounting)', hi: 'लेखाशास्त्र भाग १ (वित्तीय लेखांकन)' },
         cover: { en: 'images/acc1_en_cover.jpg', hi: 'images/acc1_hi_cover.jpg' },
         chapters: [
-            { name: { en: 'CH-1: Introduction to Accounting', hi: 'अध्याय-१: लेखांकन का परिचय' }, url_en: 'https://drive.google.com/file/d/1SgxxxE6ckcNR8pm9Km77zGV2x68zSQTj/view?usp=drivesdk/preview', url_hi: 'https://drive.google.com/file/d/1OGG8G8pibJtBdmeDAQOLcwFeM0IsBm4k/view?usp=drivesdk/preview' },
+            { name: { en: 'CH-1: Introduction to Accounting', hi: 'अध्याय-१: लेखांकन का परिचय' }, url_en: 'https://drive.google.com/file/d/1SgxxxE6ckcNR8pm9Km77zGV2x68zSQTj/preview', url_hi: 'https://drive.google.com/file/d/1OGG8G8pibJtBdmeDAQOLcwFeM0IsBm4k/preview' },
             { name: { en: 'CH-2: Theory Base of Accounting', hi: 'अध्याय-२: लेखांकन के सैद्धांतिक आधार' }, url_en: 'YAHAN_ENGLISH_LINK', url_hi: 'YAHAN_HINDI_LINK' },
             { name: { en: 'CH-3: Recording of Transactions - I', hi: 'अध्याय-३: लेन-देन का अभिलेखन - १' }, url_en: 'YAHAN_ENGLISH_LINK', url_hi: 'YAHAN_HINDI_LINK' },
             { name: { en: 'CH-4: Recording of Transactions - II', hi: 'अध्याय-४: लेन-देन का अभिलेखन - २' }, url_en: 'YAHAN_ENGLISH_LINK', url_hi: 'YAHAN_HINDI_LINK' },
